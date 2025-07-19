48102af9218999802508d67c1f4330b9cce7c16a
luuvla1610 <urrealaura5c@gmail.com>
2025-07-18 20:37:14 -0500

    Create Laura Fernanda Urrea Alcalá.java

---
+++ Laura Fernanda Urrea Alcalá.java
@@ -0,0 +1,124 @@
+public static void main(String[] args) {
+        System.out.println("Imprima su nombre y edad");
+     
+        System.out.println(" Ingrese nombre");
+        String nombre="Laura Fernanda Urrea Alcalá";
+         System.out.println(" Nombre:" +nombre);
+        System.out.println( " Ingrese edad");
+        int edad=15;
+        System.out.println(" Edad:" + edad);
+        
+        System.out.println("Calcula la suma de dos numeros");
+        int a=20;
+        int b=35;
+        System.out.println(a+b);
+        
+        System.out.println("Resta dos numeros");
+        long q=78;
+        long d=43;
+        System.out.println(q-d);
+        
+        System.out.println("Multiplica dos numeros");
+        byte y=57;
+        byte h=20;
+        System.out.println(y*h);
+        
+          System.out.println("Divide dos numeros");
+          int y=50;
+          int m=31;
+          System.out.println(y/m);
+          
+          System.out.println("Calcula el cuadrado de un numero.");
+          double z=3.4;
+          double mn=(u*u);
+          System.out.println(mn);
+          
+          System.out.println("Celsius a Fahrenheit.");
+          int wa=34;
+          int wb=7;
+          int jd=8;
+          int jc=23;
+          System.out.println(wa*wb/jd+jc);
+          
+          System.out.println("perimetro de un rectangulo");
+          byte f=62;
+          byte x=15;
+          byte la=62;
+          byte k=15;
+          System.out.println(f+x+la+k);
+          
+          System.out.println( "area de un circulo.");
+          double g=5.12;
+          int ro= 12;
+          System.out.println(g*(ro*ro));
+          
+          System.out.println("Convierte años a dias");
+          int año=35;
+          int dia=365;
+          int yam=1;
+          System.out.println(año*dia/yam);
+          
+          
+          System.out.println(" doble y el triple de un número ingresado");
+          System.out.println("Ingresa el doble");
+         int o=40;
+         System.out.println(o+o);
+          System.out.println("Ingresa el triple");
+          System.out.println(l+l+l);
+          
+          System.out.println(" kilómetros a metros y centímetros.");
+                    System.out.println("kilometros a metros");
+          int v=60;
+          int w=1000;
+          System.out.println(v*w);
+          System.out.println("metros a centimetros");
+          int qe=60000;
+          int te=100;
+          System.out.println(qe*te);
+          
+          System.out.println("area y el perimetro de un cuadrado");
+           
+           System.out.println("Ingresa el area");
+           
+           double lado=7.3;
+           System.out.println(lado*lado);
+           
+            System.out.println("Ingresa el perimetro");
+            byte perimetro=16;
+            byte exponente=3;
+            System.out.println(perimetro*exponente);
+            
+             System.out.println("Volumen de un cubo");  
+             
+             int vol=25;
+             int elev=2;
+             System.out.println(vol*elev);
+             
+             System.out.println("Volumen de una esfera");
+             
+             byte fi=5/6;
+             double co=3.16;
+             byte av=14;
+             byte bot=3;
+             System.out.println(fi*co*av*bot);
+             
+             System.out.println("Suma los tres primeros numeros naturales");
+          int if=1;
+          int af=2;
+          int uf=3;
+          System.out.println(if+af+uf);
+          
+          System.out.println("Calcula el descuento aplicado a una compra (precio y % de descuento).");
+          
+          int precio=580000;
+          double porcentaje=0.50;
+          System.out.println(precio*porcentaje);
+          System.out.println( "Insertar precio final");
+          System.out.println(580000-precio*porcentaje);
+          
+          System.out.println("Calcula la propina a dejar en un restaurante (total y % deseado)");
+          int totalcuenta=20000;
+          double porcentajepropina=3.45 ;
+          System.out.println(totalcuenta*porcentajepropina);
+          System.out.println("Insertar valor final");
+          System.out.println(45000+totalcuenta*porcentajepropina);
