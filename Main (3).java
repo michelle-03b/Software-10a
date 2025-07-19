ba0296f87019cbae63df9f10c5361f48a1c05f6b
Jaraba777 <julian.jaraba1806@gmail.com>
2025-07-18 20:38:34 -0500

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,95 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+         String nombre= "Julian Stiven Jaraba Maldonado";
+        String edad="15 años";
+        int a = 20;
+        int b = 30;
+        int c = 144;
+        int d = 12;
+        int celcius = 5;
+        int lado1 = 13;
+        int lado2 = 15;
+        int radio = 15;
+        double pi = 3.14159265358979323846;
+        double div = 4.0/3;
+        double descuento = 0.29;
+        int años = 6;
+        int dias = 365;
+        int km = 23;
+        System.out.println("Primer Punto: Edad y Nombre");
+        System.out.println("Mi nombre es " + nombre);
+        System.out.println("Mi edad es: " + edad);
+System.out.println("            ");
+        System.out.println("Segundo Punto: La suma de 20 + 30");
+        System.out.println("La suma es " + (a + b));
+System.out.println("            ");
+       System.out.println("Tercer Punto: La resta de 144 - 12");
+       System.out.println("La resta es " + (c - d));
+System.out.println("            ");                
+       System.out.println("Cuarto Punto: La multiplicaciòn de 20 * 12");
+       System.out.println("La multiplicaciòn es " + (a * d));
+System.out.println("            ");       
+       System.out.println("Quinto Punto: La divisiòn de 144 entre 12");
+       System.out.println("La divisiòn es: " + (c / d));
+System.out.println("            ");
+       System.out.println("Sexto Punto: El cuadrado de 12");
+       System.out.println("El cuadrado de 12: " + d*d);
+System.out.println("            ");      
+       System.out.println("Septimo Punto: Convertir grados Celcius a Fahrenheit");
+       System.out.println("5 grados celcius a Fahrenheit son: " + ((celcius * 9/5) + 32));
+System.out.println("            ");       
+       System.out.println("Octavo Punto: Perimetro de un rectàngulo");
+       System.out.println("El perimetro de un rectàngulo de " + lado1 + "cm de altura"+ lado2 + "cm");
+       System.out.println(lado1 + lado1 + lado2 + lado2);
+System.out.println("            ");       
+       System.out.println("Noveno Punto: Calcular el àrea de un cìrculo");
+       System.out.println("Radio = " + radio);
+       System.out.println("La àrea del circulo es " + pi * (radio * radio));
+System.out.println("            ");       
+       System.out.println("Decimo punto: Convertir  4 años a Dìas");
+       System.out.println((años * dias) +  " dias");
+System.out.println("            ");       
+       System.out.println("Undecimo punto: Calcular el doble y el triple de un nùmero ingresado: 2");
+       System.out.println("El doble de 2 es " + (2*2) + " y el triple de 2 es " + (2*3) );
+System.out.println("            ");
+       System.out.println("Doceavo punto: Convertir kilómetros a metros y centímetros: " + km + " kilómetros");
+       System.out.println(km + "Kilómetros a metros son: " + (km * 1000) + " metros ");
+       System.out.println(km + "kilómetros a centímetros son: " + (km * 100000) + " centímetros ");
+System.out.println("            ");
+       System.out.println("Treceavo punto: Calcular el área y el perimetro de un cuadrado que sus lados midan " + lado2 + " metros.");
+       System.out.println("El área de este cuadrado mide: " + (lado2 * lado2) + " metros cuadrados");
+       System.out.println("El perimetro de este cuadrado mide " + (lado2 + lado2 + lado2 + lado2) + " metros");
+System.out.println("            ");
+       System.out.println("Catorceavo punto: Calcular el volumen de un cubo donde sus lados midan: " + lado2 + "centímetros.");
+       System.out.println("El volumen del cubo es igual a: " + (lado2 * lado2 * lado2 ) + " centímetros cubicos.");
+System.out.println("            ");
+       System.out.println("Quinceavo punto: Calcular el volumen de una esfera, donde su radio mida: " + lado1 + " centímetros: ");
+       System.out.println("El volumen de esta esfera es igual a: " + (div * pi * lado1 * lado1 * lado1  ) +  " centímetros cubicos. ");
+System.out.println("            ");
+       System.out.println("Dieciseisavo punto: Sumar los primeros 3 números naturales: ");
+       System.out.println("El resultado es igual a: " + ( 1 + 2 + 3));
+System.out.println("            ");
+       System.out.println("Diecisieteavo punto: Calcular el 29 % descuento aplicado a una compra de 152 mil pesos");
+       System.out.println("El descuento a aplicado a la compra es igual a una reducción de precio de: " + (152000 * descuento) + " mil pesos.");
+       System.out.println("Es decir la compra es igual a: " + (152000 - (152000 * descuento) + " mil pesos."));
+System.out.println("            ");
+       System.out.println("Dieciochoavo punto: Calcular la propina a dejar en un restaurante (total y % deseado)");
+       System.out.println("Imaginemos que la cuenta en un restaurante es igual a 290 mil pesos, y se quiere dejar una propina del 29 %" );
+       System.out.println("Se dejaria como propina: " + (290000 * descuento) + " mil pesos");
+        
+    
+
+        
+
+
+
+        
+    }
+}
+
+
