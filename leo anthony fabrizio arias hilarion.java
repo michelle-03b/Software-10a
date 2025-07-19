ba18a616ed8876536db49e4529999f382c443041
leo0510xd <leo.arias1023@gmail.com>
2025-07-18 20:15:38 -0500

    Add files via upload

---
+++ leo anthony fabrizio arias hilarion.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Admin
+ */
+public class leoarias  {
+    public static void main(String[] args) {
+        System.out.println("-Ejercicio 1, EDAD Y NOMBRE");
+        System.out.println("leo anthony fabrizio arias hilarion"); 
+
+        int edad = 15;
+        System.out.println(edad);
+
+        int P = 35;
+        int E = 48;
+        System.out.println("-Ejercicio 2, SUMA");
+        System.out.println("35 + 48");
+        System.out.println(P + E);
+
+        int R = 89;
+        int T = 47;
+        System.out.println("-Ejercicio 3, RESTA");
+        System.out.println("89 - 47");
+        System.out.println(R - T);
+
+        int Y = 10;
+        int U = 21;
+        System.out.println("-Ejercicio 4, MULTIPLICACION");
+        System.out.println("10 * 21");
+        System.out.println(Y * U);
+
+        System.out.println("-Ejercicio 5, DIVISION");
+        System.out.println("400 / 100");
+        int I = 400;
+        int O = 100;
+        System.out.println(I / O);
+
+        System.out.println("-Ejercicio 6, CUADRADO");
+        int Z = 5;
+        System.out.println("5^2");
+        System.out.println(Z * Z);
+
+        System.out.println("-Ejercicio 7, GRADOS");
+        System.out.println("15ºC * 9/5 + 32");
+        int x = 15;
+        System.out.println((x * 9 / 5 + 32) + " fahrenheit");
+
+        System.out.println("-Ejercicio 8, PERIMETRO DE UN RECTANGULO");
+        System.out.println("Altura = 10, Base = 15");
+        int c = 10;
+        int v = 15;
+        System.out.println(c + c + v + v);
+
+        System.out.println("-Ejercicio 9, AREA DE UN CIRCULO");
+        System.out.println("Radio = 12 cm");
+        System.out.println("3.14 * 12^2");
+        double pl = 3.14;
+        int pñ = 12; 
+        System.out.println(pl * pñ * pñ + " Área Total");
+
+        System.out.println("-Ejercicio 10, AÑOS A DÍAS");
+        System.out.println("10 años * 365 días");
+        int h = 10;
+        int j = 365;
+        System.out.println(h * j);
+
+        System.out.println("-Ejercicio 11, CALCULAR EL DOBLE Y EL TRIPLE");
+        System.out.println("DOBLE DE 15");
+        int pk = 2;
+        int pq = 3;
+        System.out.println(x * pk);
+        System.out.println("TRIPLE DE 15");
+        System.out.println(x * pq);
+
+        System.out.println("-Ejercicio 12, CONVERTIR KM A METROS Y CENTIMETROS");
+        int km = 3;
+        System.out.println("3 km en metros: " + (km * 1000) + " m");
+        System.out.println("3 km en centímetros: " + (km * 100000) + " cm");
+
+        System.out.println("-Ejercicio 13, ÁREA Y PERÍMETRO DE UN CUADRADO");
+        int lado = 6;
+        System.out.println("Lado = 6");
+        System.out.println("Área = " + (lado * lado));
+        System.out.println("Perímetro = " + (lado * 4));
+
+        System.out.println("-Ejercicio 14, VOLUMEN DE UN CUBO");
+        int arista = 4;
+        System.out.println("Arista = 4");
+        System.out.println("Volumen = " + (arista * arista * arista));
+
+        System.out.println("-Ejercicio 15, VOLUMEN DE UNA ESFERA");
+        int radio = 5;
+        double pi = 3.1416;
+        double volumenEsfera = (4.0 / 3.0) * pi * Math.pow(radio, 3);
+        System.out.println("Radio = 5");
+        System.out.println("Volumen = " + volumenEsfera);
+
+        System.out.println("-Ejercicio 16, SUMAR LOS 3 PRIMEROS NÚMEROS NATURALES");
+        int sumaNaturales = 1 + 2 + 3;
+        System.out.println("1 + 2 + 3 = " + sumaNaturales);
+
+        System.out.println("-Ejercicio 17, CALCULAR DESCUENTO");
+        double precio = 100000;
+        double descuento = 0.15; // 15%
+        double montoDescontado = precio * descuento;
+        double precioFinal = precio - montoDescontado;
+        System.out.println("Precio original = " + precio);
+        System.out.println("Descuento del 15% = " + montoDescontado);
+        System.out.println("Precio con descuento = " + precioFinal);
+
+        System.out.println("-Ejercicio 18, CALCULAR PROPINA EN UN RESTAURANTE");
+        double cuenta = 75000;
+        double porcentajePropina = 0.10; // 10%
+        double propina = cuenta * porcentajePropina;
+        System.out.println("Cuenta total = " + cuenta);
+        System.out.println("Propina del 10% = " + propina);
+        System.out.println("Total con propina = " + (cuenta + propina));
+    }
+}
+
+    
+}
