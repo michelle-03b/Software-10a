63e7c99185053884b6a98c6ea57ac0d50e0734c5
XxAndresWesternxX <88463710+XxAndresWesternxX@users.noreply.github.com>
2025-07-25 18:55:53 -0500

    Add files via upload

---
+++ Andres Felipe Garcia Quevedo.java
@@ -0,0 +1,198 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+
+        
+    System.out.println("Ejercicios");
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Primer Ejercicio"+"|"+"Nombre"+"=");
+    String AE=("Andres Felipe Garcia Quevedo");
+    System.out.println(AE);
+    System.out.println("");  
+    System.out.println("-"+"Segundo Ejercicio"+"|"+"Edad");
+    int edad=15;
+    System.out.println("edad"+"="+edad);
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Segundo Ejercicio"+"|"+"Suma"+"=");    
+    System.out.println("27"+"+"+"76"+"=");
+    int G=27;
+    int Q=76;
+    System.out.println(G + Q);
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Tercer Ejercicio"+"|"+"Resta"+"=");
+    System.out.println("46"+"-"+"32"+"=");
+    int A=46;
+    int F=32;
+    System.out.println(A - F);
+    System.out.println("--------------------------------------------------------------"); 
+        
+    System.out.println("-"+"Cuarto Ejercicio"+"|"+"Multiplicacion"+"=");    
+    System.out.println("25"+"x"+"3"+"=");  
+    int N= 25;
+    int S= 3;
+    System.out.println(N * S);
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Quinto Ejercicio"+"|"+"Division"+"-");   
+     System.out.println("1052"+"/"+"4"+"=");
+    int C=1052;
+    int R=4;
+    System.out.println(C / R);
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Sexto Ejercicio"+"|"+"Calcular el cuadrado de un numero"+"="); 
+    System.out.println("150"+"^"+"2"+"=");
+    int L=150;
+    int K=150;
+    System.out.println(L * K);
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Septimo Ejercicio"+"|"+"Convertir grados Celsius a Fahrenheit"+"="); 
+    System.out.println("Grados Celcius= 55º");
+    System.out.println("(55 x 9/5)+32=");
+    int M=55;
+    int V=9;
+    int Ñ=5;
+    int O=32;
+    System.out.println((M * V/Ñ) + O);
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Octavo Ejercicio"+"|"+"Perimetro de un rectangulo"+"=");
+    System.out.println("Lado 1=10");
+    System.out.println("Lado 2=5");
+    System.out.println("Lado 3=8");
+    System.out.println("10m"+"+"+"5m"+"+"+"8m"+"=");
+    int W=10;
+    int Z=5;
+    int Y=8;
+    System.out.println(W+Z+Y);
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Noveno Ejercicio"+"|"+"Calcular el area de un circulo"+"=");
+    System.out.println("Radio=14");
+    System.out.println("π"+"x"+"14"+"+"+"^2"+"=");
+    double op=3.14;
+    int z=14;
+    int y=14 ;      
+    System.out.println(op * z * y);
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Decimo Ejercicio"+"|"+"Convertir años en dias"+"=");
+    System.out.println("1 años"+"="+"365");
+    int f= 2;
+    int h=365;
+    System.out.println(f * h);
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Undecimo Ejercicio"+"|"+"Calcular el doble y el triple de un numero ingresado"+"=");
+    System.out.println("7"+"x"+"2");
+    System.out.println("15"+"x"+"3");
+    int a=2;
+    int u=15;
+    int g=3;
+    System.out.println(u*a);
+    System.out.println(u*g);
+    System.out.println("--------------------------------------------------------------");
+
+    System.out.println("-"+"Decimo Segundo Ejercicio"+"|"+"Convertir kilometros a metro y centimetros"+"=");
+    System.out.println("1 kilometro"+" "+"x"+" "+"1000"+"="+"metros");
+    System.out.println("5 kilometro"+" "+"x"+" "+"1000"+"="+"?");
+    int U=5;
+    int D=1000;
+    System.out.println(U * D);
+    System.out.println("1 metro"+" "+"x"+" "+"100"+"="+"centimetros");
+    System.out.println("12 metro"+" "+"x"+" "+"100"+"="+"?");
+    int Pc=12;
+    int Ar=100;
+    System.out.println(Pc*Ar);
+
+    System.out.println("--------------------------------------------------------------");
+
+    System.out.println("-"+"Decimo Tercero Ejercicio"+"|"+"Calcular el area y el perimetro de un cuadrado"+"=");   
+    System.out.println("Area"+" "+"="+" "+"L1"+" "+"x"+" "+"L2");
+    System.out.println("-L1 = 7"+"   "+ "-L2 = 7");
+    System.out.println("L1"+" "+"x"+" "+"L2"+" "+"=");
+    int Na= 7;
+    int An= 7;
+    System.out.println(Na * An);
+    System.out.println("Perimetro"+" "+"="+" "+"L1"+" "+"+"+" "+"L2"+" "+"+"+" "+"L1"+" "+"+"+" L2");
+    int AN= 7;
+    int MI= 7;
+    int BU= 7;
+    int PA= 7;    
+    System.out.println(AN + MI + BU + PA);
+    System.out.println("--------------------------------------------------------------");
+
+    System.out.println("-"+"Decimo Cuarto Ejercicio"+"|"+"Calcular el volumen de un cubo"+"=");
+    System.out.println("Volumen= L x L x L");
+    System.out.println("L= 14   -   Volumen de un cubo con lado de 14 cm=");
+    int BA= 14;
+    int RC= 14;
+    int EL= 14;
+    System.out.println(BA * RC * EL);
+    System.out.println("--------------------------------------------------------------");
+
+    System.out.println("-"+"Decimo Quinto Ejercicio"+"|"+"Calcular el volumen de una esfera"+"=");
+    System.out.println("Volumen= (4/3) x π x r^3");
+    System.out.println("Encontrar radio=");
+    System.out.println("radio= diametro/2=");
+    System.out.println("Diametro= 15 cm");
+    System.out.println("radio= 15/2=");
+    double Bo=15;
+    double ca=2;
+    System.out.println(Bo / ca);
+    System.out.println("Volumen= (4/3) x π x 7.5^3=");
+    double LI= 4;
+    double VE= 3;
+    double rp= 3.141592;
+    double oo= 421.87;
+    System.out.println(LI / VE * rp * oo);
+    System.out.println("--------------------------------------------------------------");
+        
+    System.out.println("-"+"Decimo Sexto Ejercicio"+"|"+"Sumar los tres primeros numeros naturales"+"=");
+    System.out.println("1+2+3=");
+    int HJ=1;
+    int JH=2;
+    int GK=3;
+    System.out.println(HJ + JH + GK);
+    System.out.println("--------------------------------------------------------------");
+
+    System.out.println("-"+"Decimo Septimo Ejercicio"+"|"+"Calcular el descuento aplicado a una compra (precio y descuento) "+"=");
+    System.out.println("Carro= 89'000.000");
+    System.out.println("Descuento= 18%");
+    System.out.println("-Valor del porcentaje=");
+    System.out.println("18 / 100 x 89'000.000");
+    System.out.println("16'036.200");
+    System.out.println("-Restar valor del porcentaje por el valor total=");
+    System.out.println("16'036.200-89'000.000");
+    System.out.println("-Valor final con descuento=");
+    System.out.println("72'963.800");
+    System.out.println("--------------------------------------------------------------");
+
+    System.out.println("-"+"Decimo Octavo Ejercicio"+"|"+"Calcular la propina a dejar en un restaurante (total y %deseado) "+"=");
+    System.out.println(" Cuenta Tres Salchipapas=90000");
+    System.out.println(" Propina a la compra= 9%");
+    System.out.println("Valor de la propina");
+    System.out.println("9/100 x 90000=");
+    System.out.println("8100");
+    System.out.println("Valor Final de la Compra=");
+    int AS=8100;
+    int Sa=90000;
+    System.out.println(AS + Sa);
+    System.out.println("--------------------------------------------------------------");
+
+    }
+}
+  
+    
+    
+    
+    
+   
