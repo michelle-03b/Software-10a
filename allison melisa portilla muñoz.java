4bd342d71ff3b1493467b152a88f01b3290544ee
meliportilla <melisa323000234000@gmail.com>
2025-07-26 09:33:49 -0500

    allison melisa portilla muñoz.java

---
+++ allison melisa portilla muñoz.java
@@ -0,0 +1,110 @@
+System.out.println("Ejercicio uno");
+String name= Allison Melissa Portilla Muñoz;
+String age= 15;
+System.out.println("mi nombre es" + name);
+System.out.println("mi edad es" + age );
+System.out.println("Ejercicio dos");
+int f= 900;
+int l= 500;
+int h= (f+l);
+System.out.println(h);
+       
+System.out.println("Ejercicio tres");
+int k= 700;
+int w= 500;
+int p= (k-w);
+System.out.println(p);
+       
+System.out.println("Ejercicio cuatro");
+int q= 5;
+int n= 6;
+int v= (q*n);
+System.out.println(v);
+       
+System.out.println("Ejercicio cinco");
+int ñ= 600;
+int d= 6;
+int i= (ñ/d);
+System.out.println(i);
+       
+System.out.println("Ejercicio seis");  
+int M=5;
+int D=5;    
+int A=M*D;
+System.out.println(A);
+ 
+System.out.println("Ejercicio siete");
+int C=5;
+int F=41;
+int V= C=F;
+System.out.println(V);
+   
+System.out.println("Ejercicio ocho");
+int K= 6;
+int G= 3;
+int R= ((K+G)*2);
+System.out.println(R);
+   
+System.out.println("Ejercicio nueve");
+double B= 3.1416;
+int H= 9;
+System.out.println((H*H)*B);
+   
+System.out.println("Ejercicio diez");
+int L= 9;
+int Ñ=365;
+System.out.println(L*Ñ);
+   
+System.out.println("Ejercicio once");
+int T= 7;
+int W= (T+T);
+int Y=3;
+int J= (Y+Y+Y);
+System.out.println(W);
+System.out.println(J);
+
+System.out,println("Ejercicio doce");
+Int kilometros= 7
+Int  metros= 7000
+Int  centímetros=700000
+System.out.println(kilómetros=metros=centímetros);
+
+System.out,println("Ejercicio trece");
+Int base= 8
+Int altura= 8
+Int área cuadrado= (base*altura);
+System.out,println(área cuadrado);
+Int lado1= 6
+Int lado2= 6
+Int lado3= 6
+Int lado4= 6
+Int perimetro cuadrado= (lado1+lado2+lado3+lado4);
+System.out,println(perimetro cuadrado);
+
+System.out,println("Ejercicio catorce");
+Int área de cara= 14
+Int volumen cubo= (área de cara*6);
+
+System.out,println("Ejercicio quince");
+Int radio= 8
+Int volumen esfera= (4*3,14* radio);
+System.out,println(volumen esfera);
+
+System.out,println("Ejercicio dieciseis");
+Int nna=1
+Int nnb=2
+Int nnc=3
+Int suma nn= (nna+nnb+nnc);
+System.out,println(suma nn);
+
+System.out,println("Ejercicio ");
+Int pn= 18.000
+Int des 5%= 900
+Int pf= (pn-des5%);
+System.out,println(pn);
+
+System.out,println("Ejercicio diecisiete");
+Int pn= 20000
+Int pro10% = 2000
+Int pt= (pn+pro10%);
+System.out,println(pt);
