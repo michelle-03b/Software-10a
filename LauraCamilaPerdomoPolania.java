24e0e27eff1902636c742a9eb49a675df60f5e13
camilap2022 <polasan1970@gmail.com>
2025-07-26 10:47:48 -0500

    Add files via upload

---
+++ LauraCamilaPerdomoPolania.java
@@ -0,0 +1,136 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lauracamilaperdomopolania;
+
+/**
+ *
+ * @author Camila
+ */
+public class LauraCamilaPerdomoPolania {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        System.out.println("1.Imprimir tu nombre y tu edad.");
+        String Name= "Laura Camila Perdomo Polania.";
+        String Age= "15.";
+        System.out.println("Mi nombre es : "+Name);
+        System.out.println("Mi edad es: "+Age);
+        
+        System.out.println("2. Calcular la suma de 2 numeros.");
+        int J=22;
+        int C=54;
+        int JC=J+C;
+        System.out.println("22+54="+JC);
+        System.out.println("3. Restar 2 numeros ingresados por el usuario.");
+        double Z=18.32;
+        double D=38.15;
+        System.out.println("18.32-38.15="+(Z-D));
+        System.out.println("4. Multiplicar 2 numeros y mostrar el resultado.");
+        int ca=41;
+        int to=4;
+        int pe=ca*to;
+        System.out.println("41*4="+pe);
+        System.out.println("5. Dividir 2 numeros y mostrar el cociente.");
+        int Sa=1767;
+        int Sh=12;
+        int K=Sa/Sh;
+        System.out.println("1767/12="+K);
+        System.out.println("6. Calcular el cuadrado de un numero.");
+        int Da=32;
+        int Perro=Da*Da;
+        System.out.println("32*32="+Perro);
+        System.out.println("7. Covertir grados Celsius a Fahrenheit.");
+        double Car=1.8;
+        int Cat=95;
+        int Dog=32;
+        System.out.println("(1.8*95)+32="+(Car*Cat)+Dog);
+        System.out.println("8. Calcular el perimetro de un rectangulo.");
+        int B=16;
+        int H=25;
+        int BH=2*(B+H);
+        System.out.println("2*(16+25)="+BH);
+        System.out.println("9. Calcular el area de un circulo.");
+        double Pi=3.14;
+        int r=3;
+        double rosa= (Pi*(r*r));
+        System.out.println("3.14*(3*3)="+rosa);
+        System.out.println("10. Convertit años a dias.");
+        int años=17;
+        int end=años*365;
+        System.out.println("17*365="+end);
+        System.out.println("11. Calcular el doble y el triple de un numero ingresado.");
+        int casa=59;
+        int doble=2*(casa);
+        int triple=3*(casa);
+        System.out.println("2*(59)="+doble+" doble del numero");
+        System.out.println("3*(59=)"+triple+" triple del numero");
+        System.out.println("12. Convertir kilómetro a metros y centímetros.");
+        int num=7;
+        int km1=num*(1000/1);
+        int km2=num*(1000/1)*(100/1);
+        System.out.println("kilometros a metros= 7*(1000/1)="+km1);
+        System.out.println("kilometros a centimetros= 7*(1000/1)*(100/1)="+km2);
+        System.out.println("13. Calcular el área y el perímetro de un cuadrado.");
+        int bf=4;
+        int ar=bf*bf;
+        int prm=bf+bf+bf+bf;
+        System.out.println("Área= 4*4="+ar);
+        System.out.println("Per+imetro= 4+4+4+4="+prm);
+        System.out.println("14. Calcular el volumen de un cubo.");
+        int holi=56;
+        int by=holi*holi*holi;
+        System.out.println("56*56*56="+by);
+        System.out.println("15. Calcular el volumen de una esfera.");
+        int hi=7;
+        double bubble= 4/3*3.14*(hi*hi*hi);
+        System.out.println("4/3*3.14*(7*7*7)="+bubble);
+        System.out.println("16. Sumar los tres primeros números naturales.");
+        int one=1;
+        int two=2;
+        int three=3;
+        int nn=one+two+three;
+        System.out.println("1+2+3="+nn);
+        System.out.println("17. Calcular el descuento aplicado a una compra (precios y % de descuento)");
+        int Preciooriginal=50;
+        int Porcentajededescuento=20;
+        double Decimal=Porcentajededescuento*0.01;
+        double Descuento1=Preciooriginal*Decimal;
+        double Preciocondescuento=Preciooriginal-Descuento1;
+        System.out.println("Decimal= 20/100="+Decimal);
+        System.out.println("Decuento= 50*0.20="+Descuento1);
+        System.out.println("Precio con descuento= 50-10="+Preciocondescuento);
+        System.out.println("18. Calcular la propina a dejar en un restsurante (total y % deseado).");
+        int Por=20;
+        int Po=50;
+        double Porm= Por*0.01;
+        double Propi=Po*Porm;
+        double Total=Po+Propi;
+        System.out.println("Porcentaje= 12*0.01="+Porm);
+        System.out.println("Propina=50*0.12="+Propi);
+        System.out.println("Total a pagar=50+10="+Total);
+        
+        
+        
+                
+        
+               
+        
+             
+        
+     
+        
+                
+        
+              
+         
+        
+        
+        
+   
+    }
+ 
+}
