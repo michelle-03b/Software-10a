cddb9ef7108b616497a56aa11aa26abd3ad84b3d
camilax0714 <camilagutierrez1407@gmail.com>
2025-07-26 12:47:41 -0500

    Add files via upload

---
+++ LauraCamilaGutierrezPaipilla.java
@@ -0,0 +1,278 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lauracamilagutierrezpaipilla;
+
+/**
+ *
+ * @author Kelly Paipilla
+ */
+public class LauraCamilaGutierrezPaipilla {
+
+    public static void main(String[] args) {
+        
+    int A = 1;
+    int B = 2;
+    int C = 3 ;   
+    int D = 4;
+    int E = 5;
+    int F = 6;
+    int G = 7;
+    int H = 8;
+    int I = 9;
+    int J = 10;
+    int K = 11;
+    int L = 12;
+    
+   System.out.println("Tablas de Multiplicar");
+   System.out.println("Tabla del 1");
+   
+   System.out.println("1x1="+ A*A);
+   System.out.println("1x2="+ A*B);
+   System.out.println("1x3="+ A*C);
+   System.out.println("1x4="+ A*D);
+   System.out.println("1x5="+ A*E);
+   System.out.println("1x6="+ A*F);
+   System.out.println("1x7="+ A*G);
+   System.out.println("1x8="+ A*H);
+   System.out.println("1x9="+ A*I);
+   System.out.println("1x10="+ A*J);
+   System.out.println("1x11="+ A*K);
+   System.out.println("1x12="+ A*L);
+   
+   System.out.println("Tabla del 2");
+   
+   System.out.println("2x1="+ B*A);
+   System.out.println("2x2="+ B*B);
+   System.out.println("2x3="+ B*C);
+   System.out.println("2x4="+ B*D);
+   System.out.println("2x5="+ B*E);
+   System.out.println("2x6="+ B*F);
+   System.out.println("2x7="+ B*G);
+   System.out.println("2x8="+ B*H);
+   System.out.println("2x9="+ B*I);
+   System.out.println("2x10="+ B*J);
+   System.out.println("2x11="+ B*K);
+   System.out.println("2x12="+ B*L);
+   
+   System.out.println("Tabla del 3");
+   
+   System.out.println("3x1="+ C*A);
+   System.out.println("3x2="+ C*B);
+   System.out.println("3x3="+ C*C);
+   System.out.println("3x4="+ C*D);
+   System.out.println("3x5="+ C*E);
+   System.out.println("3x6="+ C*F);
+   System.out.println("3x7="+ C*G);
+   System.out.println("3x8="+ C*H);
+   System.out.println("3x9="+ C*I);
+   System.out.println("3x10="+ C*J);
+   System.out.println("3x11="+ C*K);
+   System.out.println("3x12="+ C*L);
+   
+   System.out.println("Tabla del 4");
+   
+   System.out.println("4x1="+ D*A);
+   System.out.println("4x2="+ D*B);
+   System.out.println("4x3="+ D*C);
+   System.out.println("4x4="+ D*D);
+   System.out.println("4x6="+ D*F);
+   System.out.println("4x7="+ D*G);
+   System.out.println("4x8="+ D*H);
+   System.out.println("4x9="+ D*I);
+   System.out.println("4x10="+ D*J);
+   System.out.println("4x11="+ D*K);
+   System.out.println("4x12="+ D*L);
+   
+   System.out.println("Tabla del 5");
+   
+   System.out.println("5x1="+ E*A);
+   System.out.println("5x2="+ E*B);
+   System.out.println("5x3="+ E*C);
+   System.out.println("5x4="+ E*D);
+   System.out.println("5x5="+ E*E);
+   System.out.println("5x6="+ E*F);
+   System.out.println("5x7="+ E*G);
+   System.out.println("5x8="+ E*H);
+   System.out.println("5x9="+ E*I);
+   System.out.println("5x10="+ E*J);
+   System.out.println("5x11="+ E*K);
+   System.out.println("5x12="+ E*L);
+   
+   System.out.println("Tabla del 6");
+   
+   System.out.println("6x1="+ F*A);
+   System.out.println("6x2="+ F*B);
+   System.out.println("6x3="+ F*C);
+   System.out.println("6x4="+ F*D);
+   System.out.println("6x5="+ F*E);
+   System.out.println("6x6="+ F*F);
+   System.out.println("6x7="+ F*G);
+   System.out.println("6x8="+ F*H);
+   System.out.println("6x9="+ F*I);
+   System.out.println("6x10="+ F*J);
+   System.out.println("6x11="+ F*K);
+   System.out.println("6x12="+ F*L);
+   
+   System.out.println("Tabla del 7");
+   
+   System.out.println("7x1="+ G*A);
+   System.out.println("7x2="+ G*B);
+   System.out.println("7x3="+ G*C);
+   System.out.println("7x4="+ G*D);
+   System.out.println("7x5="+ G*E);
+   System.out.println("7x6="+ G*F);
+   System.out.println("7x7="+ G*G);
+   System.out.println("7x8="+ G*H);
+   System.out.println("7x9="+ G*I);
+   System.out.println("7x10="+ G*J);
+   System.out.println("7x11="+ G*K);
+   System.out.println("7x12="+ G*L);
+   
+   System.out.println("Tabla del 8");
+   
+   System.out.println("8x1="+ H*A);
+   System.out.println("8x2="+ H*B);
+   System.out.println("8x3="+ H*C);
+   System.out.println("8x4="+ H*D);
+   System.out.println("8x5="+ H*E);
+   System.out.println("8x6="+ H*F);
+   System.out.println("8x7="+ H*G);
+   System.out.println("8x8="+ H*H);
+   System.out.println("8x9="+ H*I);
+   System.out.println("8x10="+ H*J);
+   System.out.println("8x11="+ H*K);
+   System.out.println("8x12="+ H*L);
+   
+   System.out.println("Tabla del 9");
+   
+   System.out.println("9x1="+ I*A);
+   System.out.println("9x2="+ I*B);
+   System.out.println("9x3="+ I*C);
+   System.out.println("9x4="+ I*D);
+   System.out.println("9x5="+ I*E);
+   System.out.println("9x6="+ I*F);
+   System.out.println("9x7="+ I*G);
+   System.out.println("9x8="+ I*H);
+   System.out.println("9x9="+ I*I);
+   System.out.println("9x10="+ I*J);
+   System.out.println("9x11="+ I*K);
+   System.out.println("9x12="+ I*L);
+   
+   System.out.println("Tabla del 10");
+   
+   System.out.println("10x1="+ J*A);
+   System.out.println("10x2="+ J*B);
+   System.out.println("10x3="+ J*C);
+   System.out.println("10x4="+ J*D);
+   System.out.println("10x5="+ J*E);
+   System.out.println("10x6="+ J*F);
+   System.out.println("10x7="+ J*G);
+   System.out.println("10x8="+ J*H);
+   System.out.println("10x9="+ J*I);
+   System.out.println("10x10="+ J*J);
+   System.out.println("10x11="+ J*K);
+   System.out.println("10X12="+ J*L);
+  
+   System.out.println("Tabla del 11");
+   
+   System.out.println("11x1="+ J*A);
+   System.out.println("11x2="+ J*B);
+   System.out.println("11x3="+ J*C);
+   System.out.println("11x4="+ J*D);
+   System.out.println("11x5="+ J*E);
+   System.out.println("11x6="+ J*F);
+   System.out.println("11x7="+ J*G);
+   System.out.println("11x8="+ J*H);
+   System.out.println("11x9="+ J*I);
+   System.out.println("11x10="+ J*J);
+   System.out.println("11x11="+ J*K);
+   System.out.println("11X12="+ J*L);
+  
+  System.out.println("Tabla del 12");
+   
+   System.out.println("12x1="+ L*A);
+   System.out.println("12x2="+ L*B);
+   System.out.println("12x3="+ L*C);
+   System.out.println("12x4="+ L*D);
+   System.out.println("12x5="+ L*E);
+   System.out.println("12x6="+ L*F);
+   System.out.println("12x7="+ L*G);
+   System.out.println("12x8="+ L*H);
+   System.out.println("12x9="+ L*I);
+   System.out.println("12x10="+ L*J);
+   System.out.println("12x11="+ L*K);
+   System.out.println("12X12="+ L*L);
+        
+    
+    
+ System.out.println("Calcular si un año es bisiesto");
+
+        int año =2000;
+
+        if (año % 4 == 0) {
+        if (año % 100 == 0) {
+        if (año % 400 == 0) {
+            System.out.println("el año:" + año +" es bisiesto");
+        } else {
+            System.out.println("el año :" + año + "no es bisiesto");
+        }
+    } else {
+        System.out.println("el año :"+ año + "es bisiesto");
+    }
+} else {
+    System.out.println("El año"+ año + "no es bisiesto");
+}
+    System.out.println(" ");   
+    
+ ///**///***////**////***////**/////****/////////****//////****////**////**////  
+    System.out.println("La serie de Fibonacci");
+    
+     int a=0;
+     int b=1;
+ 
+    for (int i=2; i < 30; i++){
+      int c=a+b;
+  
+    System.out.println(c);
+    a=b;
+    b=c;   
+    }        
+    
+    System.out.println("Dado un numero cualquiera imprimir dicho numero hasta el limite 30");
+      int  x = 1;
+      if (x > 30){
+          System.out.println(" El numero es Mayor a 30");
+      } else{
+        for ( int w = x; w <=30; w++) {
+          System.out.println(w);
+        }     
+      }
+      
+    ///****///*******//////*********/////////******//////******/////*********/////*****////*
+    
+    System.out.println("Cantidad de numeros impares que hay dentro de un rango");
+
+     int inicio = 5;
+        int fin = 83;
+        int Impares = 0; // Contar y mostrar los números impares dentro del rango
+        System.out.print("Números impares= ");
+        for (int i = inicio; i <= fin; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+                Impares++;   
+                }  
+            }
+      
+      
+      
+      
+      
+    }
+    
+     
+
+}
+    
+  
