3ff27675c6220ace620941dc4711e4931596fa04
DylanG2807 <dylanguiza2807@gmail.com>
2025-07-25 21:49:56 -0500

    Add files via upload

---
+++ dylanguiza.java
@@ -0,0 +1,118 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Dylan Guiza ");
+        System.out.println("-------------------------------------------");
+      System.out.println("   SUMA   ");
+     int a=5;
+     int b=7;         
+    
+    System.out.println("la suma de 5+7 es"); System.out.println(a+b);
+     
+     int x= 10;
+     int y= 5;
+    System.out.println("-------------------------------------------"); 
+     System.out.println("RESTA");
+      System.out.println("la resta de 10-5 es");
+  System.out.println(x-y);
+  
+System.out.println("-------------------------------------------");
+  System.out.println("MULTIPLICACION");
+    
+    int r= 5;
+    int t= 5;
+    System.out.println("la multiplicacion de 5*5 es");
+    System.out.println(r*t);
+System.out.println("-------------------------------------------");    
+
+System.out.println("DIVISION");
+    
+    int L= 100;
+    int O= 2;
+    
+    System.out.println("la division de 100 dividido 2 es");
+    
+    System.out.println(100/2);
+        System.out.println("-------------------------------------------");
+        
+        System.out.println("CUADRADO");
+        
+        int n= 5;
+          
+                System.out.println("el cuadrado de 5 es");
+           System.out.println(5*5);
+        System.out.println("-------------------------------------------");
+        
+        System.out.println("GRADOS CELSIUS A FAHENHEIT");
+        System.out.println("40 GRADOS CELSIUS A FAHRENHEIT");
+        
+        System.out.println(40*9/5+32);
+        System.out.println("-------------------------------------------");
+        
+        System.out.println("CALCULO DE PERIMETRO DE UN RECTANGULO");
+ System.out.println("el perimetro de un rectangulo de 9*15 es");      
+        
+        int largo=9;
+                int ancho=15;
+                        System.out.println(2*(largo+ancho));
+        
+                        System.out.println("-------------------------------------------");
+                        System.out.println("AREA DE UN CIRCULO");
+                        double pi=3.14; 
+                        System.out.println(pi*(20*20));
+        
+                       System.out.println("CONVERTIR AÑOS EN DIAS");
+                       int año=365;
+                       int numero=8;
+                       System.out.println("6 años equivalen a");        
+                       System.out.println(8*36);
+                       
+                       System.out.println("-------------------------------------------");
+                       
+                       System.out.println("EL DOBLE Y EL TRIPLE DE UN NUMERO INGRESADO");
+                       System.out.println(7*2);
+                       System.out.println(7*3);
+
+
+
+
+System.out.println(" Calcular el área y el perímetro de un cuadrado.");  
+    int lado= 2;
+    int lado2= 2;
+    System.out.println("perimetro");
+       System.out.println(2*4);
+    System.out.println("area");
+    System.out.println(4*4);
+
+System.out.println("Calcular el volumen de un cubo."); 
+    double ladot=4;
+    int volumen =lado0*ladotladot; 
+    System.out ("lado del + lado0);  
+    System.out.println("volumen del cubo: " + volumen + " );  
+
+System.out.println("Calcular el volumen de una esfera.");
+    double volumem=(4/3)*pi*radio*radio*radio;        
+    System.out.println("el volumen de la esfera es de "+volumem); 
+
+System.out.println("Suma los tres primeros números naturales."); 
+    int n1= 1;
+    int n2= 2; 
+    int n3= 3; 
+  System.out.printl(n1+n2+n3);
+
+
+System.out.println("descuento a una compra"); 
+
+int p1= 2.000
+int d1= 50%
+int E= 100
+
+System.out.println(2.000*(d1/E)); 
+
+
+    }
+}
