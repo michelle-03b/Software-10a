4f7e79f844c82f3483ad5bed91d81d25b80e800c
Gabrielaortiz33 <gabrielaortizog1003@gmail.com>
2025-07-25 23:36:22 -0500

    Add files via upload

---
+++ Gabriela Alexandra Rodríguez Ortiz (Ejercicios de Recursión).java
@@ -0,0 +1,207 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "GabrielaAlexandraRodríguezOrtiz".
+class Main {
+    public static void main(String[] args) {
+            int ab= 0;
+    int bc= 1;
+    int cd= 2;
+    int de= 3;
+    int ef= 4;
+    int fg= 5;
+    int gh= 6;
+    int hi= 7;
+    int ij= 8;
+    int jk= 9;
+    int kl= 10;
+System.out.println(" "); 
+System.out.println(" ");  
+System.out.println("Tabla de multiplicar hasta el diez");
+System.out.println("Tabla multiplicar del uno");
+System.out.println("1x1="+bc*bc);
+System.out.println("1x2="+bc*cd);
+System.out.println("1x3="+bc*de);
+System.out.println("1x4="+bc*ef);
+System.out.println("1x5="+bc*fg);
+System.out.println("1x6="+bc*gh);
+System.out.println("1x7="+bc*hi);
+System.out.println("1x8="+bc*ij);
+System.out.println("1x9="+bc*jk);
+System.out.println("1x10="+bc*kl);
+System.out.println(" "); 
+System.out.println("Tabla multiplicar del dos");
+System.out.println("2x1="+cd*bc);
+System.out.println("2x2="+cd*cd);
+System.out.println("2x3="+cd*de);
+System.out.println("2x4="+cd*ef);
+System.out.println("2x5="+cd*fg);
+System.out.println("2x6="+cd*gh);
+System.out.println("2x7="+cd*hi);
+System.out.println("2x8="+cd*ij);
+System.out.println("2x9="+cd*jk);
+System.out.println("2x10="+cd*kl);
+System.out.println(" "); 
+System.out.println("Tabla multiplicar del tres");
+System.out.println("3x1="+de*bc);
+System.out.println("3x2="+de*cd);
+System.out.println("3x3="+de*de);
+System.out.println("3x4="+de*ef);
+System.out.println("3x5="+de*fg);
+System.out.println("3x6="+de*gh);
+System.out.println("3x7="+de*hi);
+System.out.println("3x8="+de*ij);
+System.out.println("3x9="+de*jk);
+System.out.println("3x10="+de*kl);
+System.out.println(" "); 
+System.out.println("Tabla multiplicar del cuatro");
+System.out.println("4x1="+ef*bc);
+System.out.println("4x2="+ef*cd);
+System.out.println("4x3="+ef*de);
+System.out.println("4x4="+ef*ef);
+System.out.println("4x5="+ef*fg);
+System.out.println("4x6="+ef*gh);
+System.out.println("4x7="+ef*hi);
+System.out.println("4x8="+ef*ij);
+System.out.println("4x9="+ef*jk);
+System.out.println("4x10="+ef*kl);
+System.out.println(" "); 
+System.out.println("Tabla multiplicar del cinco");
+System.out.println("5x1="+fg*bc);
+System.out.println("5x2="+fg*cd);
+System.out.println("5x3="+fg*de);
+System.out.println("5x4="+fg*ef);
+System.out.println("5x5="+fg*fg);
+System.out.println("5x6="+fg*gh);
+System.out.println("5x7="+fg*hi);
+System.out.println("5x8="+fg*ij);
+System.out.println("5x9="+fg*jk);
+System.out.println("5x10="+fg*kl);
+System.out.println(" "); 
+System.out.println("Tabla multiplicar del seis");
+System.out.println("6x1="+gh*bc);
+System.out.println("6x2="+gh*cd);
+System.out.println("6x3="+gh*de);
+System.out.println("6x4="+gh*ef);
+System.out.println("6x5="+gh*fg);
+System.out.println("6x6="+gh*gh);
+System.out.println("6x7="+gh*hi);
+System.out.println("6x8="+gh*ij);
+System.out.println("6x9="+gh*jk);
+System.out.println("6x10="+gh*kl);
+System.out.println(" "); 
+System.out.println("Tabla multiplicar del siete");
+System.out.println("7x1="+hi*bc);
+System.out.println("7x2="+hi*cd);
+System.out.println("7x3="+hi*de);
+System.out.println("7x4="+hi*ef);
+System.out.println("7x5="+hi*fg);
+System.out.println("7x6="+hi*gh);
+System.out.println("7x7="+hi*hi);
+System.out.println("7x8="+hi*ij);
+System.out.println("7x9="+hi*jk);
+System.out.println("7x10="+hi*kl);
+System.out.println(" "); 
+System.out.println("Tabla multiplicar del ocho");
+System.out.println("8x1="+ij*bc);
+System.out.println("8x2="+ij*cd);
+System.out.println("8x3="+ij*de);
+System.out.println("8x4="+ij*ef);
+System.out.println("8x5="+ij*fg);
+System.out.println("8x6="+ij*gh);
+System.out.println("8x7="+ij*hi);
+System.out.println("8x8="+ij*ij);
+System.out.println("8x9="+ij*jk);
+System.out.println("8x10="+ij*kl);
+System.out.println(" "); 
+System.out.println("Tabla multiplicar del nueve");
+System.out.println("9x1="+jk*bc);
+System.out.println("9x2="+jk*cd);
+System.out.println("9x3="+jk*de);
+System.out.println("9x4="+jk*ef);
+System.out.println("9x5="+jk*fg);
+System.out.println("9x6="+jk*gh);
+System.out.println("9x7="+jk*hi);
+System.out.println("9x8="+jk*ij);
+System.out.println("9x9="+jk*jk);
+System.out.println("9x10="+jk*kl);
+System.out.println(" "); 
+System.out.println("Tabla multiplicar del diez");
+System.out.println("10x1="+kl*bc);
+System.out.println("10x2="+kl*cd);
+System.out.println("10x3="+kl*de);
+System.out.println("10x4="+kl*ef);
+System.out.println("10x5="+kl*fg);
+System.out.println("10x6="+kl*gh);
+System.out.println("10x7="+kl*hi);
+System.out.println("10x8="+kl*ij);
+System.out.println("10x9="+kl*jk);
+System.out.println("10x10="+kl*kl);
+System.out.println(" "); 
+System.out.println(" "); 
+System.out.println("Calcular si un año es bisiesto");   
+int año = 2005;
+if (año % 4 == 0) {
+if (año % 100 == 0) {
+if (año % 400 == 0) {
+System.out.println("el año es bisiesto");
+} else {
+System.out.println("el año no es bisiesto");
+}
+} else {
+System.out.println("el año es bisiesto");
+}
+} else {
+System.out.println("el año no es bisiesto");
+}
+System.out.println(" ");
+System.out.println(" ");
+System.out.println("La serie Fibonacci");
+int a=0;
+int b=1;
+int c=a+b;
+int d=b+c;
+int e=c+d;
+int f=d+e;
+int g=e+f;
+int h=f+g;
+int i=g+h;
+int j=h+i;
+int k=i+j;
+int l=j+k;
+int m=k+l;
+int n=l+m;
+int ñ=m+n;
+int o=n+ñ;
+int p=ñ+o;
+int q=o+p;
+int r=p+q;
+int s=q+r;        
+System.out.println(a+","+b+","+c+","+d+","+e+","+f+","+g+","+h+","+i+","+j+","+k+","+l+","+m+","+n+","+ñ+","+o+","+p+","+q+","+r+","+s);
+System.out.println(" ");
+System.out.println(" ");
+System.out.println("Dado un número x, imprima todos los números de dicho hasta el limite y"); 
+int xx = 2; // número inicial
+int yy = 18; // número final
+while (xx <= yy) {
+System.out.println(xx);
+xx+=xx;
+    }
+        // Cantidad de números impares dentro de un rango
+        System.out.println("La cantidad de números que hay dentro de un rango");
+        int inicio = 5;
+        int fin = 20;
+        int cantidadImpares = 0;
+
+        while (inicio <= fin) {
+            if (inicio % 2 != 0) {
+                cantidadImpares++;
+            }
+            inicio++;
+        }
+
+        System.out.println("Cantidad de números impares: " + cantidadImpares);
+    }
+}
+}
