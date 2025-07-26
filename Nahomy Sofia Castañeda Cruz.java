1e7731dab7e08735e4be70bb3299de37ca7e00e8
Nahomy1502 <castanedanahomy22@gmail.com>
2025-07-25 20:41:20 -0500

    Nahomy Sofia Castañeda Cruz.java

---
+++ Nahomy Sofia Castañeda Cruz.java
@@ -0,0 +1,262 @@
+public class NahomyJava {
+
+    public static void main(String[] args) {
+System.out.println("TABLA DEL 1");
+       
+        int A=1;
+       
+        System.out.println("1*1"+"="+A*A);
+       
+        int B=2;
+       
+        System.out.println("1*2"+"="+A*B);
+       
+        int C=3;
+       
+        System.out.println("1*3"+"="+A*C);
+       
+        int D=4;
+       
+        System.out.println("1*4"+"="+A*D);
+       
+        int E=5;
+       
+        System.out.println("1*5"+"="+A*E);
+       
+        int F=6;
+       
+     
+        System.out.println("1*6"+"="+A*F);
+     
+       
+       int G=7;
+       
+       System.out.println("1*7"+"="+A*G);
+       
+       int H=8;
+       
+       System.out.println("1*8"+"="+A*H);
+       
+       int W=9;
+       
+       System.out.println("1*9"+"="+A*W);
+       
+       int X=10;
+       
+       System.out.println("1*10"+"="+A*X);
+       
+       int  P=11;
+           System.out.println("1*11"+"="+A*P);
+           
+           int Y=12;
+                   
+                   System.out.println("1*12"+"="+A*Y);
+                   
+                   System.out.println("TABLA DEL 2");
+           
+               System.out.println("2*1"+"="+B*A);
+                  System.out.println("2*2"+"="+B*B);
+                   System.out.println("2*3"+"="+B*C);
+                    System.out.println("2*4"+"="+B*D);
+                     System.out.println("2*5"+"="+B*E);
+                      System.out.println("2*6"+"="+B*F);
+                       System.out.println("2*7"+"="+B*G);
+                        System.out.println("2*8"+"="+B*H);
+                         System.out.println("2*9"+"="+B*W);
+                          System.out.println("2*10"+"="+B*X);
+                          System.out.println("2*11"+"="+B*P);
+                          System.out.println("2*12"+"="+B*Y);
+                         
+                           System.out.println("TABLA DEL 3");
+           
+                          System.out.println("3*1"+"="+C*A);    
+                         
+                          System.out.println("3*2"+"="+C*B);  
+                         
+                          System.out.println("3*3"+"="+C*C);      
+                         
+                          System.out.println("3*4"+"="+C*D);  
+                         
+                          System.out.println("3*5"+"="+C*E);    
+                         
+                          System.out.println("3*6"+"="+C*F);    
+                         
+                          System.out.println("3*7"+"="+C*G);  
+                         
+                          System.out.println("3*8"+"="+C*H);        
+                         
+                          System.out.println("3*9"+"="+C*W);  
+                         
+                          System.out.println("3*10"+"="+C*X);
+                         
+                          System.out.println("3*11"+"="+C*P);
+                          System.out.println("3*12"+"="+C*Y);
+                         
+                          System.out.println("TABLA DEL 4");
+                         
+                            System.out.println("4*1"+"="+D*A);
+                           
+                            System.out.println("4*2"+"="+D*B);
+                           
+                            System.out.println("4*3"+"="+D*C);
+                           
+                            System.out.println("4*4"+"="+D*D);
+                           
+                            System.out.println("4*5"+"="+D*E);
+                           
+                            System.out.println("4*6"+"="+D*F);
+                           
+                            System.out.println("4*7"+"="+D*G);
+                           
+                            System.out.println("4*8"+"="+D*H);
+                           
+                            System.out.println("4*9"+"="+D*W);
+                           
+                            System.out.println("4*10"+"="+D*X);
+                           
+                            System.out.println("4*11"+"="+D*P);
+                           
+                            System.out.println("4*12"+"="+D*Y);
+                           
+                                System.out.println("TABLA DEL 5");
+                               
+                               
+                            System.out.println("5*1"+"="+E*A);
+                                     System.out.println("5*2"+"="+E*B);
+                                  System.out.println("5*3"+"="+E*C);
+                                   System.out.println("5*4"+"="+E*D);
+                 System.out.println("5*5"+"="+E*E);
+                 System.out.println("5*6"+"="+E*F);
+                     System.out.println("5*7"+"="+E*G);
+                           System.out.println("5*8"+"="+E*H);
+                           System.out.println("5*9"+"="+E*W);
+                           System.out.println("5*10"+"="+E*X);
+                           System.out.println("5*11"+"="+E*P);
+                           System.out.println("5*12"+"="+E*Y);
+                         
+                  System.out.println("TABLA DEL 6");
+                 
+                 
+                            System.out.println("6*1"+"="+F*A);
+                              System.out.println("6*2"+"="+F*B);
+                   System.out.println("6*3"+"="+F*C);
+                   System.out.println("6*4"+"="+F*D);
+                   System.out.println("6*5"+"="+F*E);
+                   System.out.println("6*6"+"="+F*F);
+                   System.out.println("6*7"+"="+F*G);
+                   System.out.println("6*8"+"="+F*H);
+                   System.out.println("6*9"+"="+F*W);
+                   System.out.println("6*10"+"="+F*X);
+                   System.out.println("6*11"+"="+F*P);
+                   System.out.println("6*12"+"="+F*Y);
+                   
+                   
+                  System.out.println("TABLA DEL 7");
+                   
+                     System.out.println("7*1"+"="+G*A);
+                    System.out.println("7*2"+"="+G*B);
+                    System.out.println("7*3"+"="+G*C);
+                    System.out.println("7*4"+"="+G*D);
+                    System.out.println("7*5"+"="+G*E);
+                    System.out.println("7*6"+"="+G*F);
+                    System.out.println("7*7"+"="+G*G);
+                    System.out.println("7*8"+"="+G*H);
+                    System.out.println("7*9"+"="+G*W);
+                    System.out.println("7*10"+"="+G*X);
+                    System.out.println("7*11"+"="+G*P);
+                     System.out.println("7*12"+"="+G*Y);
+                     
+                         System.out.println("TABLA DEL 8");
+                         
+                     System.out.println("8*1"+"="+H*A);    
+                       System.out.println("8*2"+"="+H*B);
+                       System.out.println("8*3"+"="+H*C);
+                       System.out.println("8*4"+"="+H*D);
+                       System.out.println("8*5"+"="+H*E);
+                       System.out.println("8*6"+"="+H*F);
+                       System.out.println("8*7"+"="+H*G);
+                       System.out.println("8*8"+"="+H*H);
+                       System.out.println("8*9"+"="+H*W);
+                       System.out.println("8*10"+"="+H*X);
+                          System.out.println("8*11"+"="+H*P);
+                          System.out.println("8*12"+"="+H*Y);
+                     
+                     System.out.println("TABLA DEL 9");
+                       
+                              System.out.println("9*1"+"="+W*A);    
+                       System.out.println("9*2"+"="+W*B);
+                       System.out.println("9*3"+"="+W*C);
+                       System.out.println("9*4"+"="+W*D);
+                       System.out.println("9*5"+"="+W*E);
+                       System.out.println("9*6"+"="+W*F);
+                       System.out.println("9*7"+"="+W*G);
+                       System.out.println("9*8"+"="+W*H);
+                       System.out.println("9*9"+"="+W*W);
+                       System.out.println("9*10"+"="+W*X);
+                          System.out.println("9*11"+"="+W*P);
+                          System.out.println("9*12"+"="+W*Y);
+                       
+                        System.out.println("TABLA DEL 10");
+                       
+                       
+                               System.out.println("10*1"+"="+X*A);    
+                       System.out.println("10*2"+"="+X*B);
+                       System.out.println("10*3"+"="+X*C);
+                       System.out.println("10*4"+"="+X*D);
+                       System.out.println("10*5"+"="+X*E);
+                       System.out.println("10*6"+"="+X*F);
+                       System.out.println("10*7"+"="+X*G);
+                       System.out.println("10*8"+"="+X*H);
+                       System.out.println("10*9"+"="+X*W);
+                       System.out.println("10*10"+"="+X*X);
+                          System.out.println("10*11"+"="+X*P);
+                          System.out.println("10*12"+"="+X*Y);
+
+                           System.out.print("AÑO BISIESTO");
+       
+    double ka=1996.0;
+    int multiplo=4;
+    int divi=100;
+    int divi1=400;
+    
+    System.out.println("primer resultado=" +ka/multiplo);
+    System.out.println("segundo resultado=" +ka/divi);
+    System.out.println("tercer resultado=" +ka/divi1);
+   
+  System.out.println("ESTE AÑO NO ES BISIESTO");
+  
+  System.out.println("NUMEROS DE LA RESIE FIBONACCI");
+   
+  int hj=30;
+  int vb=0; 
+   int bv=1;
+            for (int i = 1; i <= hj; i++) {
+                System.out.print(bv + " ");
+                int siguiente = vb + bv;
+                vb = bv;
+                bv = siguiente;
+                
+                System.out.println("MULTIPLO DE UN NUMERO");
+                
+                     long ini=3;
+                     long fin=50;
+                   while (ini<=fin) {  
+                       
+                   }
+                       System.out.println(ini);
+                       ini+=ini; 
+                       
+                       System.out.println("cantidad de numeros impares");
+                       
+                       int za= 6;
+                       int zo=30;
+                       int lo=1;
+                       int re=0;
+                       
+                        while (lo <= zo) {
+            if (lo % 2 != 0) {
+                re++;
+            }
+            lo++;
+                               
+           System.out.println("números impares entre " + za + " y " + zo + ": " + re);
