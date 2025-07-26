c722c6e7802734814f5c3ea97738d73223d8dc44
Nightr01M <andresfhernandez406@gmail.com>
2025-07-26 11:37:02 -0500

    Add files via upload

---
+++ Andres Hernandez ejercicios recursion.java
@@ -0,0 +1,111 @@
+package tablas.de.mulitplicar;
+
+public class Andres Hernandez ejercicios recursion {
+    
+   public static void main(String[] args) {
+  for (int tabla = 1; tabla<=10; tabla++) {
+         System.out.println("Tabla del " +tabla );
+         for (int i = 1; i <= 12;i++){ //Cada tabla va del 1 al 12
+             System.out.println( tabla + "x" + i + "=" + (tabla*i));
+             System.out.println();//Salto de linea entre tablas
+        
+    
+           
+             
+             
+   
+    //System.out.println(diviaion);
+    int año=2024;
+    if (año%4==0) {
+        
+        if(año%400==0) {
+            System.out.println("el año es biciesto");
+            
+            
+        }
+    }
+             boolean condicion = false;
+    if(condicion) {
+        
+    } else{
+                 
+                 System.out.println("el año no es biciesto");
+  
+      //
+    //System.out.println(Fibonacci);
+    int a=0, b=1;
+    System.out.print(a + "," + b);
+    for ( int i = 3; i <= 20;i++){
+        int c = a + b;
+        a = b;       
+        b = c;
+        
+       
+    }
+    
+    System.out.println();
+    }
+   }
+    */
+   System.out.println("Multiplos numero x");
+   int x= 5;//Numero base 
+   int y= 200;//Limite
+   System.out.println("Multiplos de " + x + "hasta" + y + ":");
+   
+   for (int i = x; i <= y; i +=x){
+       System.out.println(i);
+   }
+   }
+}
+   
+   
+   int limite= 25; //Se puede cambiar por cualquier numero
+   System.out.println("Numeros impares del 1 al " + limite + ":");
+   for (int i = 1; i <= limite; i++){
+       
+       if (i % 2 != 0 ){
+           System.out.println( i + " ");
+       }
+       }
+   }
+  
+  
+  
+
+               
+    
+    
+
+    
+    
+   
+    
+    
+                 
+                 
+         
+         
+         
+         
+         
+         
+         
+         
+         }
+     }
+   }
+}
+
+
+
+
+
+
+
+
+             
+             
+
+             
+    
+
